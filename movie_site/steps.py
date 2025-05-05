2a0c008735244733e8fab24728f8b6ab8e04cd46
Admin <mrrneo@outlook.com>
2025-05-05 15:47:47 +0600

    ...

--- movie_site/steps.py
+++ movie_site/steps.py
@@ -85,5 +85,28 @@ RUN pip install --upgrade pip && \
 COPY nginx/nginx.conf /etc/nginx/conf.d/
 
 COPY . /app/
-5.0)
+5.0) EC2 --> Instance --> проекттин аты, ubuntu, free, key --> Security Groups --> Edit inbound rules --> Add rule: 8000 и 80
+6.0) EC2 --> Instance --> проектке киребиз --> Connect --> Connect
+7.0) sudo apt-get update
+8.0) sudo apt-get upgrade
+9.0) git clone [path]
+10) sudo apt install python3.12-venv
+11) python3 -m venv venv
+12) source venv/bin/activate
+13) sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+14) sudo chmod +x /usr/local/bin/docker-compose
+15) docker-compose --version
+16) sudo apt install apt-transport-https ca-certificates curl software-properties-common
+17) curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+18) sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+19) apt-cache policy docker-ce
+20) sudo apt install docker-ce
+21) sudo systemctl status docker
+22) завершим настройку CTRL + C
+23) проекттин ичине киребиз
+24) chmod +x manage.py
+25) sudo docker-compose up --build -d
+26) sudo docker-compose ps
+27) sudo docker-compose logs
+
 
